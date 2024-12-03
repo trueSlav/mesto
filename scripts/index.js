74ad9e7f5f262be7e86e23ad652325ac132e5b5e
trueSlav <2001idercach@gmail.com>
2024-12-03 14:26:58 +0300

    feat(render-cards): displaying cards on the screen

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,35 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content
+const cardsDelButtonElement = cardTemplate.querySelector('.card__delete-button').cloneNode(true)
+console.log(cardsDelButtonElement)
+
+const removeCard = (e) => {
+    console.log(e)
+    e.setAttribute('data-hidden', 'hidden')
+}
+
+const addCard = ({name, link}) => {
+    const cardElementList = document.querySelector('.places__list')
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true)
+    const cardImageElement = cardElement.querySelector('.card__image')
+    const cardTitleElement = cardElement.querySelector('.card__title')
+
+    cardImageElement.src = link
+    cardImageElement.alt = name
+    cardTitleElement.textContent = name
+
+    cardElementList.append(cardElement)
+}
+
+initialCards.forEach(card => {
+    addCard(card)
+})
+
+// cardsDelButtonElement.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//         console.log(e.target)
+//     })
+// })
