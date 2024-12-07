7e58e85b7bc5124965b0169de10bbb93d47e5cc2
trueSlav <2001idercach@gmail.com>
2024-12-08 01:08:06 +0300

    build: the code formatter is configured

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,38 @@
 const cardElementList = document.querySelector('.places__list')
 
-const cardItems = () => {
-    const cardTemplate = document.querySelector('#card-template').content
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
-    const cardImageElement = cardElement.querySelector('.card__image')
-    const cardTitleElement = cardElement.querySelector('.card__title')
+const selectCardItems = () => {
+	const cardTemplate = document.querySelector('#card-template').content
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+	const cardImageElement = cardElement.querySelector('.card__image')
+	const cardTitleElement = cardElement.querySelector('.card__title')
 
-    return {
-        li: cardElement,
-        imageCard: cardImageElement,
-        titleCard: cardTitleElement,
-    }
+	return {
+		cardElement,
+		cardImageElement,
+		cardTitleElement
+	}
 }
 
-const renderItems = (parent, renderElement, data) => {
-    const {name, link} = data;
+const renderItems = (parent, renderElement, removeCard, data) => {
+	const { name, link } = data
 
-    renderElement.imageCard.src = link
-    renderElement.imageCard.alt = name
-    renderElement.titleCard.textContent = name
+	renderElement.cardImageElement.src = link
+	renderElement.cardImageElement.alt = name
+	renderElement.cardTitleElement.textContent = name
 
-    parent.append(renderElement.li)
+	removeCard(parent)
+
+	return parent.append(renderElement.cardElement)
+}
+
+const removeItem = parent => {
+	parent.addEventListener('click', e => {
+		if (e.target && e.target.classList.contains('card__delete-button')) {
+			e.target.parentElement.remove()
+		}
+	})
 }
 
 initialCards.forEach(card => {
-    renderItems(cardElementList, cardItems(), {...card})
+	renderItems(cardElementList, selectCardItems(), removeItem, { ...card })
 })
-
-const removeItem = (parent) => {
-    parent.addEventListener('click', e => {
-        if(e.target && e.target.classList.contains('card__delete-button')) {
-            e.target.parentElement.remove()
-        }
-    })
-
-}
-removeItem(cardElementList)
