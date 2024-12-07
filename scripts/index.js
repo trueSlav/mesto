a389d9232c89ced407177c44b8b829558b8a4b29
trueSlav <2001idercach@gmail.com>
2024-12-08 02:07:18 +0300

    feat(first check): changed the function of deleting, creating a card

--- scripts/index.js
+++ scripts/index.js
@@ -5,34 +5,40 @@ const selectCardItems = () => {
 	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
 	const cardImageElement = cardElement.querySelector('.card__image')
 	const cardTitleElement = cardElement.querySelector('.card__title')
+	const cardDeleteButtonElement = cardElement.querySelectorAll(
+		'.card__delete-button'
+	)
 
 	return {
 		cardElement,
 		cardImageElement,
-		cardTitleElement
+		cardTitleElement,
+		cardDeleteButtonElement
 	}
 }
 
-const renderItems = (parent, renderElement, removeCard, data) => {
+const createCard = (parent, renderElement, data) => {
 	const { name, link } = data
 
 	renderElement.cardImageElement.src = link
 	renderElement.cardImageElement.alt = name
 	renderElement.cardTitleElement.textContent = name
 
-	removeCard(parent)
+	renderElement.cardDeleteButtonElement.forEach(button => {
+		button.addEventListener('click', () =>
+			removeItem(renderElement.cardElement)
+		)
+	})
 
-	return parent.append(renderElement.cardElement)
+	return renderElement.cardElement
 }
 
-const removeItem = parent => {
-	parent.addEventListener('click', e => {
-		if (e.target && e.target.classList.contains('card__delete-button')) {
-			e.target.parentElement.remove()
-		}
-	})
+const removeItem = cardElement => {
+	cardElement.remove()
 }
 
 initialCards.forEach(card => {
-	renderItems(cardElementList, selectCardItems(), removeItem, { ...card })
+	cardElementList.append(
+		createCard(cardElementList, selectCardItems(), { ...card })
+	)
 })
